6d1e7ad00d66c6b2c8bd70bfebaa095e7903155b
nagolove <dekar.own@gmail.com>
2021-01-26 21:35:32 +0300

    Initial.

---
+++ types.lua
@@ -0,0 +1,92 @@
+require("love")
+love.filesystem.setRequirePath("scenes/automato/?.lua")
+require("mtschemes")
+
+ Pos = {}
+
+
+
+
+ Cell = {}
+
+
+
+
+
+
+
+
+
+
+
+ CellSetup = {}
+
+
+
+
+ Cells = {}
+ Grid = {}
+ GetGridFunction = {}
+ InitCellFunction = {}
+
+ CellActionsInit = {}
+
+
+
+
+
+
+
+
+
+ Statistic = {}
+
+
+
+
+
+
+
+ DrawNode = {}
+
+
+
+
+
+ CommonSetup = {}
+
+
+
+
+
+
+
+
+
+
+
+ ThreadCommandsStore = {}
+
+
+
+
+
+
+
+
+ ThreadCommands = {}
+
+
+
+
+
+
+
+
+ SimulatorMode = {}
+
+
+
+
+
+ Simulator = {}
